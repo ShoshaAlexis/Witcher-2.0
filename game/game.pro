fadae9d57595fb655b4fa8d5e0f4671f77207443
AMiron <l.miron082002@gmail.com>
2023-05-07 21:15:00 +0300

    Base files

---
+++ game/game.pro
@@ -0,0 +1,16 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+SOURCES += main.cpp
+CONFIG +=c++11
+LIBS += -L"SFML-2.3.2\lib"
+CONFIG(release, debug|release):
+LIBS += -lsfml-audio -lsfml-graphics -lsfml-main -lsfml-network -lsfml-window -lsfml-system
+CONFIG(debug, debug|release):
+LIBS += -lsfml-audio-d -lsfml-graphics-d -lsfml-main-d -lsfml-network-d -lsfml-window-d -lsfml-system-d
+INCLUDEPATH += "SFML-2.3.2\include"
+include(deployment.pri)
+qtcAddDeployment()
+
+HEADERS +=
---
+++ game/map.h
@@ -0,0 +1,32 @@
+#include <SFML\Graphics.hpp>
+const int HEIGHT_MAP = 25;
+const int WIDTH_MAP = 40;
+
+
+std::string TileMap[HEIGHT_MAP] = {
+    "0000000000000000000000000000000000000000",
+    "0                                     s0",
+    "0                                     s0",
+    "0             ss        ss            s0",
+    "0     ss      s0        s0     ss     s0",
+    "0     s0      s0sssssssss0     s0     s0",
+    "0     s0      s000000000s0     s0     s0",
+    "0     s0      s0        s0     s0     s0",
+    "0     s0                       s0     s0",
+    "0     s0                       s0     s0",
+    "0                                     s0",
+    "0                                     s0",
+    "0ss   sssss                 sssss   sss0",
+    "000   s0000                 s0000   s0s0",
+    "0                                     s0",
+    "0             ss        ss            s0",
+    "0             s0sssssssss0            s0",
+    "0             s00000000000            s0",
+    "0   ss   ss   s0        s0   ssssss   s0",
+    "0   s0   s0   s0        s0   s00000   s0",
+    "0   s0   s0   s0        s0            s0",
+    "0                                     s0",
+    "0                                     s0",
+    "0ssssssssssssssssssssssssssssssssssssss0",
+    "0000000000000000000000000000000000000000",
+};
